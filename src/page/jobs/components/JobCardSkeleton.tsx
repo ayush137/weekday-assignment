5b25b0c77d2cb522a2ca43652836bb33c47b4afe
ayush137 <bhadouriaayush@gmail.com>
2024-05-08 21:01:50 +0530

    feat: job card skeleton integrated

--- src/page/jobs/components/JobCardSkeleton.tsx
+++ src/page/jobs/components/JobCardSkeleton.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { Paper, Skeleton, Stack } from "@mui/material";
+import styles from "../jobs.module.css";
 
 const JobCardSkeleton = () => {
-  return <div>JobCardSkeleton</div>;
+  return (
+    <>
+      {[...Array(6)]?.map(() => (
+        <Paper elevation={1} className={styles["job-card-skeleton"]}>
+          <Stack spacing={1}>
+            <Skeleton
+              variant="text"
+              sx={{ fontSize: "1rem" }}
+              height={20}
+              width={98}
+            />
+            <Skeleton variant="circular" width={40} height={40} />
+            <Skeleton variant="text" height={30} />
+            <Skeleton variant="text" height={30} />
+            <Skeleton variant="text" height={30} />
+            <Skeleton variant="text" height={30} />
+            <Skeleton variant="text" height={30} />
+            <Skeleton variant="text" height={30} />
+            <Skeleton variant="rounded" width={210} />
+            <Skeleton variant="text" height={80} />
+            <Skeleton variant="text" height={80} />
+          </Stack>
+        </Paper>
+      ))}
+    </>
+  );
 };
 
 export default JobCardSkeleton;
--- src/page/jobs/index.tsx
+++ src/page/jobs/index.tsx
@@ -3,15 +3,17 @@ import { useGetSampleJdJSONQuery } from "../../store/service/adhoc/post";
 import JobCard from "./components/JobCard";
 import styles from "./jobs.module.css";
 import useIntersectionObserver from "../../hooks/useIntersectionObserver";
+import JobCardSkeleton from "./components/JobCardSkeleton";
 
 const Jobs = () => {
   const LIMIT = 12;
   const { isIntersecting, observer, observerRef } = useIntersectionObserver();
   const [offset, setOffset] = useState(0);
-  const { data, isUninitialized, isLoading } = useGetSampleJdJSONQuery({
-    limit: 12,
-    offset: offset,
-  });
+  const { data, isUninitialized, isLoading, isFetching } =
+    useGetSampleJdJSONQuery({
+      limit: 12,
+      offset: offset,
+    });
   const hasMore = (data?.totalCount || 0) >= offset + LIMIT;
 
   useEffect(() => {
@@ -23,10 +25,6 @@ const Jobs = () => {
 
   const jobList = useMemo(() => {
     return data?.jdList?.map((item, index) => {
-      console.log(
-        "index === data?.jdList?.length - 4",
-        index === data?.jdList?.length - 4 ? observerRef : undefined
-      );
       return (
         <JobCard
           jobDetail={item}
@@ -38,7 +36,12 @@ const Jobs = () => {
     });
   }, [data?.jdList?.length]);
 
-  return <div className={styles["jobcard-wrapper"]}>{jobList}</div>;
+  return (
+    <div className={styles["jobcard-wrapper"]}>
+      {jobList}
+      {isUninitialized || isLoading || isFetching ? <JobCardSkeleton /> : null}
+    </div>
+  );
 };
 
 export default Jobs;
